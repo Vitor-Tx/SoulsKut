9b437fbfc450bf59a70e10a65d0e27af0afd6b3b
Vitor Teixeira <49378292+Vitor-Tx@users.noreply.github.com>
2021-07-20 02:16:14 -0300

    Update communities.js

--- pages/api/communities.js
+++ pages/api/communities.js
@@ -1,27 +1,3 @@
-/* import { SiteClient } from 'datocms-client';
-
-export default async function requestReceiver (request, response) {
-    const TOKEN = "f07c49dad33685f1ddea4aa454c3ee";
-    const client = new SiteClient(TOKEN);
-
-    const registroCriado = await client.items.create({
-        itemType: "978579",
-        title: "Comunidade de teste",
-        imageUrl: "https://github.com/Vitor-Tx",
-        creatorSlug: "Vitor-Tx",
-
-    })
-
-    console.log(registroCriado);
-
-    console.log(TOKEN);
-    response.json({
-        data: "dados",
-        registroCriado: registroCriado, 
-    })
-
-} */
-
 import { SiteClient } from 'datocms-client';
 
 export default async function recebedorDeRequests(request, response) {
@@ -34,9 +10,6 @@ export default async function recebedorDeRequests(request, response) {
         const registroCriado = await client.items.create({
             itemType: "978579",
             ...request.body,
-            /* title: "Comunidade de teste",
-            imageUrl: "https://github.com/Vitor-Tx",
-            creatorSlug: "Vitor-Tx", */
     
         })
     
